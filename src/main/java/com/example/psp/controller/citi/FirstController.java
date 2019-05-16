425792676a4fecda9fb818450845363cd6f4dc18
parashuram-patil <parasharam_patil@ymail.com>
2019-05-16 13:09:38 +0530

    Swagger2 Intergration
    -- @RequestMapping will prioved documentation for all HTTP methods

--- src/main/java/com/example/psp/controller/citi/FirstController.java
+++ src/main/java/com/example/psp/controller/citi/FirstController.java
@@ -1,12 +1,12 @@
 package com.example.psp.controller.citi;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FirstController {
 
-	@RequestMapping("/hello")
+	@GetMapping("/hello")
 	String home() {
 		return "Hello World!!!";
 	}
