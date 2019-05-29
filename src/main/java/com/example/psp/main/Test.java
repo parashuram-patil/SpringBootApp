4be556783871765f04a79f7d35f86b02a131cece
parashuram-patil <parasharam_patil@ymail.com>
2019-05-29 17:15:20 +0530

    Model Mapper Test Eg

--- src/main/java/com/example/psp/main/Test.java
+++ src/main/java/com/example/psp/main/Test.java
@@ -1,17 +1,38 @@
 package com.example.psp.main;
 
+import static org.junit.Assert.assertEquals;
+
+import org.modelmapper.ModelMapper;
+
+import com.example.psp.entity.citi.CityEntity;
+import com.example.psp.model.citi.CreateCityModel;
+
 public class Test {
-	
+
 	private static final String message = "City :msg not found";
 
 	public static void main(String[] args) {
 		System.out.println("**********	Test	**********");
-		
+
+		// replaceStaticString();
+		testModelMapper();
+
+	}
+
+	private static void testModelMapper() {
+		CreateCityModel cityModel = new CreateCityModel();
+		cityModel.setCityName("a");
+		cityModel.setPopulation(1);
+		CityEntity cityEntity = new ModelMapper().map(cityModel, CityEntity.class);
+		assertEquals(cityEntity.getName(), cityModel.getCityName());
+		assertEquals(cityEntity.getPopulation(), cityModel.getPopulation());
+	}
+
+	private static void replaceStaticString() {
 		System.out.println(message.replace(":msg", "XXX"));
 		System.out.println(message);
 		System.out.println(message.replace(":msg", "YYYY"));
 		System.out.println(message);
-		
 	}
 
 }
