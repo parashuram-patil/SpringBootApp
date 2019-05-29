2867747e3c813de6991c8c1dc24c30f1cd2f2467
parashuram-patil <parasharam_patil@ymail.com>
2019-05-29 15:42:34 +0530

    Different response for diffrent code
    -- @ExampleProperty wont work with API response
    -- Can be achieved with @ApiModelProperty

--- src/main/java/com/example/psp/controller/citi/CityController.java
+++ src/main/java/com/example/psp/controller/citi/CityController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.example.psp.entity.citi.CityEntity;
 import com.example.psp.iservice.city.ICityService;
+import com.psp.exception.CustomErrorResponse;
 import com.psp.exception.city.CityNotFoundException;
 
 import io.swagger.annotations.Api;
@@ -21,6 +22,8 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.Example;
+import io.swagger.annotations.ExampleProperty;
 
 @Controller
 @Api(tags = { "City" }, description = "Know your city")
@@ -67,14 +70,17 @@ public class CityController {
 		return findCities(model);
 	}
 
-	@ApiOperation(value = "Search City by its name", response = CityEntity.class)
-	@ApiResponses(value = { @ApiResponse(code = 200, message = "Successfully retrieved City with given name"),
+	@ApiOperation(value = "Search City by its name")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "Successfully retrieved City with given name", response = CityEntity.class),
+			// @ApiResponse(code = 200, message = "Successfully retrieved City with given name", response = CityEntity.class, examples = @Example(value = @ExampleProperty(value = "{\"id\":1,\"name\":\"MyCity\",\"population\":9999}", mediaType = "application/json"))),
 			@ApiResponse(code = 401, message = "You are not authorized to view the resource"),
 			@ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
-			@ApiResponse(code = 404, message = "The resource you were trying to reach is not found") })
+			@ApiResponse(code = 404, message = "The resource you were trying to reach is not found", response = CustomErrorResponse.class) })
 	@RequestMapping(value = "/searchCity/{cityName}", method = RequestMethod.GET, produces = "text/html")
 	public String searchCity(Model model,
-			@ApiParam(value = "Name of the City", required = true) @PathVariable String cityName) throws CityNotFoundException {
+			@ApiParam(value = "Name of the City", required = true) @PathVariable String cityName)
+			throws CityNotFoundException {
 
 		CityEntity foundCity = cityService.searchByName(cityName);
 
